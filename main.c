bed9d27043e4fcdf40406f3c267c50ea6314e070
omniaelagroudy0 <omniaelagroudy@gmail.com>
2023-08-23 07:48:08 -0700

    m

--- main.c
+++ main.c
@@ -1,71 +1,44 @@
-#include "main.h"
+#include "shell.h"
 
-int main(int ac, char **argv){
-    char *prompt = "(Eshell) $ ";
-    char *ln_pt = NULL, *ln_pt_copy = NULL;
-    size_t n = 0;
-    ssize_t nchars_read;
-    const char *delim = " \n";
-    int num_tokens = 0;
-    char *token;
-    int i;
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    /* declaring void variables */
-    (void)ac;
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-    /* Create a loop for the shell's prompt */
-    while (1) {
-        printf("%s", prompt);
-        nchars_read = getline(&ln_pt, &n, stdin);
-        /* check if the getline function failed or reached EOF or user use CTRL + D */
-        if (nchars_read == -1){
-            printf("Exiting shell....\n");
-            return (-1);
-        }
-
-        /* allocate space for a copy of the ln_pt */
-        ln_pt_copy = malloc(sizeof(char) * nchars_read);
-        if (ln_pt_copy== NULL){
-            perror("tsh: memory allocation error");
-            return (-1);
-        }
-        /* copy ln_pt to ln_pt_copy */
-        strcpy(ln_pt_copy, ln_pt);
-
-        /********** split the string (ln_pt) into an array of words ********/
-        /* calculate the total number of tokens */
-        token = strtok(ln_pt, delim);
-
-        while (token != NULL){
-            num_tokens++;
-            token = strtok(NULL, delim);
-        }
-        num_tokens++;
-
-        /* Allocate space to hold the array of strings */
-        argv = malloc(sizeof(char *) * num_tokens);
-
-        /* Store each token in the argv array */
-        token = strtok(ln_pt_copy, delim);
-
-        for (i = 0; token != NULL; i++){
-            argv[i] = malloc(sizeof(char) * strlen(token));
-            strcpy(argv[i], token);
-
-            token = strtok(NULL, delim);
-        }
-        argv[i] = NULL;
-
-        /* execute the command */
-        execmd(argv);
-
-    }
-
-
-    /* free up allocated memory */
-    free(ln_pt_copy);
-    free(ln_pt);
-
-    return (0);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
